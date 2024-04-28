0a4e1ae641d7b75a38fa54e8c52d75b33e726fd5
RocioPortal <p.portal@alumno.um.edu.ar>
2024-04-28 18:20:28 -0300

    primer commit para main

---
+++ main.py
@@ -0,0 +1,9 @@
+from persona import Persona
+
+if __name__ == '__main__':
+    persona = Persona()
+    print(persona.__str__())  #print(persona.__dict__) funcionalidad especial de python que me permite ver los atributos de la clase en forma de diccionario
+    persona = Persona()
+    print(persona)
+    persona = Persona()
+    print(persona)
